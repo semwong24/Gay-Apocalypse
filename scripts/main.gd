db5f2810092ce07e803255bfc5c60074af3c8ba9
SJM17 <sophiemorris9896@gmail.com>
2026-01-30 21:27:09 -0800

    Interact Ray & Scripts
    Added an interact Ray to the Player3D model, letting you look at items in order to be prompted to pick them up. Included scripts makes interactivity between items much easier, allowing for customization in the inspector. Ex; Functionality added - Flashlight cannot be toggled until in game flashlight is grabbed.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,7 +3,7 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.start("opening")
+	#Dialogic.start("opening")
 	pass # Replace with function body.
 
 
