fdddcb7882bd0319989e0f2081029300c42f4bbf
SJM17 <sophiemorris9896@gmail.com>
2026-02-03 19:21:03 -0800

    Doorknob & Requirements
    The interactable script can now be attached to any doorknob to teleport players. There is now a "requirement" function that allows you to make it so that something is locked until players have another item

--- scripts/interactable.gd
+++ scripts/interactable.gd
@@ -9,7 +9,22 @@ signal interacted(body)
 @export var unlocks_action = ""
 @export var disappears_on_pickup = true
 
+# Requirement settings
+@export_group("Requirements")
+@export var requires_item = false
+@export var required_item_name = ""
+@export var locked_message = "Locked"
+
+# teleport settings
+@export_group("Teleportation")
+@export var teleports_player = false
+@export var teleport_position: Vector3 = Vector3.ZERO
+@export var teleport_marker: Marker3D
+
 func get_prompt():
+	if requires_item and not PlayerInventory.has_item(required_item_name):
+		return locked_message
+		
 	var key_name = ""
 	for action in InputMap.action_get_events(prompt_input):
 		if action is InputEventKey:
@@ -19,10 +34,24 @@ func get_prompt():
 	return prompt_message + "\n[" + key_name + "]"
 
 func interact(body):
+	if requires_item and not PlayerInventory.has_item(required_item_name):
+		print("Need item: ", required_item_name)
+		return 
+		
 	if item_name != "":
 		PlayerInventory.pickup_item(item_name)
 	
+	if teleports_player:
+		teleport_player(body)
+	
 	interacted.emit(body)
+
+
+func teleport_player(body):
+	if body.is_in_group("player") or body.has_method("teleport"):
+		var destination = teleport_marker.global_position if teleport_marker else teleport_position
+		body.global_position = destination
+
 
 func _ready():
 	interacted.connect(_on_interacted)
