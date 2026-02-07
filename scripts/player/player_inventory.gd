b7dc7fc184db4c8a07ec24ae4d6d9dd42eb83e7d
SJM17 <sophiemorris9896@gmail.com>
2026-02-07 01:17:00 -0800

    New Interior
    Updated the starting interior with our own assets. Move around some of the items from inside the asset to be more coherent.
    
    Added a hatchet and replaced the flashlight and doorknob to still work.

--- scripts/player/player_inventory.gd
+++ scripts/player/player_inventory.gd
@@ -1,16 +1,21 @@
 extends Node
 
 var has_flashlight = false
+var has_hatchet = false
 # can add more items later
 
 func pickup_item(item_name: String):
 	match item_name:
 		"flashlight":
 			has_flashlight = true
+		"hatchet":
+			has_hatchet = true
 
 func has_item(item_name: String) -> bool:
 	match item_name:
 		"flashlight":
 			return has_flashlight
+		"hatchet":
+			return has_hatchet
 		_:
 			return false
