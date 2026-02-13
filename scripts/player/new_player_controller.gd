fc9b80c06640c12050c6de59d30e9b4dec6da2a2
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2026-02-13 11:03:05 -0800

    camera control for convenience

--- scripts/player/new_player_controller.gd
+++ scripts/player/new_player_controller.gd
@@ -20,7 +20,7 @@ func _input(event):
 		
 	if event.is_action_pressed("toggle_mouse"):
 		if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
