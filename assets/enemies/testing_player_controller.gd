f4ebdb3e912e5c43e4f40e1331677bd81d00016a
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2026-02-20 12:53:09 -0800

    trying to get the new player to work but idk lol

--- assets/enemies/testing_player_controller.gd
+++ assets/enemies/testing_player_controller.gd
@@ -87,9 +87,6 @@ func _input(event):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	
 	# Only process mouse look when cursor is captured
-	if event is InputEventMouseMotion:
-		if Input.mouse_mode != Input.MOUSE_MODE_CAPTURED:
-			return
 			
 		rotate_y(-event.relative.x * mouse_sensitivity / 100.0)
 		$Head.rotate_x(-event.relative.y * mouse_sensitivity / 100.0)
