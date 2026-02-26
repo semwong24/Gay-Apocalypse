18002414488994e2b5d6803b92b02a3528859691
SJM17 <sophiemorris9896@gmail.com>
2026-02-26 13:23:13 -0800

    Dialogue Select Fix + Ambient Sound Update
    Made the dialogue selection actually highlight the proper option, and made ambient sound louder relative to voices

--- scripts/player/dialogue_scroll.gd
+++ scripts/player/dialogue_scroll.gd
@@ -1,5 +1,4 @@
 extends Node
-
 var current_choice_index = 0
 var choice_buttons = []
 var last_button_count = 0
@@ -13,26 +12,21 @@ func _process(_delta):
 		var buttons = _find_all_buttons(layout)
 		if buttons.size() > 0:
 			choice_buttons = buttons
-
 			if buttons.size() != last_button_count:
 				current_choice_index = 0
 				last_button_count = buttons.size()
 				_highlight_choice(current_choice_index)
-				_connect_hover_signals()  # ← connect hover whenever new buttons appear
-
+				_connect_hover_signals()
 			current_choice_index = clamp(current_choice_index, 0, choice_buttons.size() - 1)
-
-			# Scroll wheel and arrow keys both navigate
 			if Input.is_action_just_pressed("dialogue_scroll_down") or Input.is_action_just_pressed("ui_down"):
 				current_choice_index = (current_choice_index + 1) % choice_buttons.size()
 				_highlight_choice(current_choice_index)
-
 			if Input.is_action_just_pressed("dialogue_scroll_up") or Input.is_action_just_pressed("ui_up"):
 				current_choice_index = (current_choice_index - 1 + choice_buttons.size()) % choice_buttons.size()
 				_highlight_choice(current_choice_index)
-
 			if Input.is_action_just_pressed("dialogue_select") or Input.is_action_just_pressed("ui_accept"):
-				choice_buttons[current_choice_index].emit_signal("choice_selected")
+				var opposite_index = (choice_buttons.size() - 1) - current_choice_index
+				choice_buttons[opposite_index].emit_signal("choice_selected")
 		else:
 			last_button_count = 0
 			current_choice_index = 0
@@ -40,7 +34,6 @@ func _process(_delta):
 func _connect_hover_signals() -> void:
 	for i in range(choice_buttons.size()):
 		var btn = choice_buttons[i]
-		# Avoid duplicate connections
 		if not btn.mouse_entered.is_connected(_on_button_hovered.bind(i)):
 			btn.mouse_entered.connect(_on_button_hovered.bind(i))
 
@@ -60,6 +53,6 @@ func _highlight_choice(index: int):
 	for i in range(choice_buttons.size()):
 		if i == index:
 			choice_buttons[i].grab_focus()
-			choice_buttons[i].modulate = Color(1.3, 1.3, 0.8)
-		else:
 			choice_buttons[i].modulate = Color.WHITE
+		else:
+			choice_buttons[i].modulate = Color(1.3, 1.3, 0.8)
