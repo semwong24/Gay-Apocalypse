d9bae7d6a7c4976f605b2fc6cbc9cb2ecb404cfc
semwong24 <serenemwong@gmail.com>
2026-02-25 18:18:43 -0800

    Dialogic Scroll fix + choice inputs

--- scripts/player/dialogue_scroll.gd
+++ scripts/player/dialogue_scroll.gd
@@ -2,6 +2,7 @@ extends Node
 
 var current_choice_index = 0
 var choice_buttons = []
+var last_button_count = 0
 
 func _ready():
 	return
@@ -12,17 +13,40 @@ func _process(_delta):
 		var buttons = _find_all_buttons(layout)
 		if buttons.size() > 0:
 			choice_buttons = buttons
-			
-			if Input.is_action_just_pressed("dialogue_scroll_down"):
+
+			if buttons.size() != last_button_count:
+				current_choice_index = 0
+				last_button_count = buttons.size()
+				_highlight_choice(current_choice_index)
+				_connect_hover_signals()  # ← connect hover whenever new buttons appear
+
+			current_choice_index = clamp(current_choice_index, 0, choice_buttons.size() - 1)
+
+			# Scroll wheel and arrow keys both navigate
+			if Input.is_action_just_pressed("dialogue_scroll_down") or Input.is_action_just_pressed("ui_down"):
 				current_choice_index = (current_choice_index + 1) % choice_buttons.size()
 				_highlight_choice(current_choice_index)
-			
-			if Input.is_action_just_pressed("dialogue_scroll_up"):
+
+			if Input.is_action_just_pressed("dialogue_scroll_up") or Input.is_action_just_pressed("ui_up"):
 				current_choice_index = (current_choice_index - 1 + choice_buttons.size()) % choice_buttons.size()
 				_highlight_choice(current_choice_index)
-			
+
 			if Input.is_action_just_pressed("dialogue_select") or Input.is_action_just_pressed("ui_accept"):
 				choice_buttons[current_choice_index].emit_signal("choice_selected")
+		else:
+			last_button_count = 0
+			current_choice_index = 0
+
+func _connect_hover_signals() -> void:
+	for i in range(choice_buttons.size()):
+		var btn = choice_buttons[i]
+		# Avoid duplicate connections
+		if not btn.mouse_entered.is_connected(_on_button_hovered.bind(i)):
+			btn.mouse_entered.connect(_on_button_hovered.bind(i))
+
+func _on_button_hovered(index: int) -> void:
+	current_choice_index = index
+	_highlight_choice(current_choice_index)
 
 func _find_all_buttons(node):
 	var buttons = []
