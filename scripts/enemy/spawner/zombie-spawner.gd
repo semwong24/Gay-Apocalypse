1c907f89e61370a510193264a5800b6df959e09e
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2026-03-06 17:57:03 -0800

    Revert "they grumpy"
    This reverts commit 0f04ec52433f4e1951ee0e65c54b70f4fbb0c678.

--- scripts/enemy/spawner/zombie-spawner.gd
+++ scripts/enemy/spawner/zombie-spawner.gd
@@ -1,14 +1,9 @@
 extends Node3D
-
-@export_group("zombie")
 @export var spawnable: PackedScene
-@export var spawn_radius:= 5
-@export var speed:= 3
-@export var player_dist_max := 50
-@export var origin_dist_max := 20
+@export var radius:float
+@export var speed: float
 var player = null
 var rng = RandomNumberGenerator.new()
-@export_group("player")
 @export var player_path : NodePath
 var zombies = []
 # Called when the node enters the scene tree for the first time.
@@ -19,14 +14,12 @@ func _ready() -> void:
 func spawn_zombie():
 	var newzombie = spawnable.instantiate()
 	add_child(newzombie)
-	var randx = rng.randf_range(position.x-spawn_radius,position.x+spawn_radius)
-	var randz = rng.randf_range(position.z-spawn_radius,position.z+spawn_radius)
+	var randx = rng.randf_range(position.x-radius,position.x+radius)
+	var randz = rng.randf_range(position.z-radius,position.z+radius)
 	newzombie.position = Vector3(randx,position.y,randz)
 	newzombie.player_path = player_path
 	newzombie.player = get_node(player_path)
 	newzombie.SPEED = speed
-	newzombie.player_dist_max = player_dist_max
-	newzombie.origin_dist_max = origin_dist_max
 	zombies.append(newzombie)
 	
 
