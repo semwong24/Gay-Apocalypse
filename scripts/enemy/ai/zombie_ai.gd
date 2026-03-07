0f04ec52433f4e1951ee0e65c54b70f4fbb0c678
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2026-03-06 17:56:15 -0800

    they grumpy

--- scripts/enemy/ai/zombie_ai.gd
+++ scripts/enemy/ai/zombie_ai.gd
@@ -1,25 +1,41 @@
 extends CharacterBody3D
 var player = null
+@export_group("player info")
 @export var player_path : NodePath
 @onready var nav_agent = $NavigationAgent3D
-@export var player_dist_max: float
+@export var player_dist_max := 50
+@export_group("zombie traits")
+@export var origin_dist_max := 20
 var origin: Vector3
-@export var SPEED = 3.0
-@export var JUMP_VELOCITY = 4.5
+@export var SPEED := 3.0
+@export var JUMP_VELOCITY := 4.5
+var rng = RandomNumberGenerator.new()
+var randomtick = 0
+var randompick = 0
 func _ready():
 	player = get_node(player_path)
 	origin = self.global_position
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	randomtick += delta
 	var player_dist = global_position.distance_to(player.global_position)
+	var origin_dist = global_position.distance_to(origin)
 	velocity = Vector3.ZERO
 	var spd = SPEED
 	if player_dist < player_dist_max:
 		nav_agent.set_target_position(player.global_transform.origin)
 	else:
-		nav_agent.set_target_position(self.origin)
-		spd = spd +10
+		if origin_dist < origin_dist_max:
+			spd = spd *0.75
+			if randomtick>=randompick:
+				var randx = rng.randf_range(origin.x-origin_dist_max,origin.x+origin_dist_max)
+				var randz = rng.randf_range(origin.z-origin_dist_max,origin.z+origin_dist_max)
+				nav_agent.set_target_position(Vector3(randx,position.y,randz))
+				randomtick = 0
+				randompick = rng.randi_range(0,30)
+		else:
+			nav_agent.set_target_position(self.origin)
+			spd = spd *5
 	
 	
 	if not is_on_floor():
