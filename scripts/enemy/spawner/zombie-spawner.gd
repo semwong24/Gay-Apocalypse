a02cf19533852a4944acbe031d77b7e83895013b
zSinani <ili18@ucsc.edu>
2026-03-09 06:17:05 -0700

    Squashed commit of the following:
    commit 0d5806de431fa9c11f8d0d55c663dc14d7ff6b9c
    Merge: 8e32216 e8c779a
    Author: zSinani <ili18@ucsc.edu>
    Date:   Mon Mar 9 05:41:14 2026 -0700
    
        Merge remote-tracking branch 'origin/um-whops-enemy-aiu-adj' into merge-testing
    
    commit e8c779a741cfd41c92fc2b78eb191686c6c95926
    Author: SJM17 <sophiemorris9896@gmail.com>
    Date:   Mon Mar 9 01:18:16 2026 -0700
    
        Zombie Count Variable
    
        lets you decide how many zombies each spawner....spawns.
        Also adjusted the size of the zombie, bro was huge.
    
    commit 2d9131cf28369dee29d9c60cbab8d6d8d815edfc
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sun Mar 8 20:56:13 2026 -0700
    
        ya bro groans
    
    commit 5760875e5d3d853219e2074e5b365a25556828e5
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sun Mar 8 20:54:23 2026 -0700
    
        revert zombie defaults
    
    commit a54526b0f79a516895f325da3c964416704f9fb5
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sun Mar 8 20:53:31 2026 -0700
    
        so heres the groaner
    
    commit b8edf3667b66185d9e96dab6173d13ab694ad118
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sun Mar 8 15:39:01 2026 -0700
    
        door fix
    
    commit e8b56dd395bfb580b068ea895503d773443bc32e
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sun Mar 8 13:50:15 2026 -0700
    
        moved player back in the house
    
    commit 05bf06d7ff19a1048d6cf03a88908c6daed0f1a8
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sun Mar 8 13:37:11 2026 -0700
    
        added a bunch of spawners so we lagmaxxing or what
    
    commit f93c5de9f3770e77fa28cfacc1b499ee06ca5ffa
    Merge: 4816142 0c3a74b
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sun Mar 8 13:20:03 2026 -0700
    
        Merge branch 'main' into um-whops-enemy-aiu-adj
    
    commit 48161420a33ab1b48a953985f0946cd41f829fac
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sat Mar 7 18:58:07 2026 -0800
    
        volkswagen jumpscare
    
    commit f9b193652c4882a98fb0432bf7e57516db379046
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sat Mar 7 18:13:39 2026 -0800
    
        this is fine???
    
    commit 8ee6e31523f66206b8b3603a70b20553c7df5667
    Author: DidSomeoneSayBoba <aureliadufromage37@gmail.com>
    Date:   Sat Mar 7 12:20:41 2026 -0800
    
        i mean in theory

---
+++ scripts/enemy/spawner/zombie-spawner.gd
@@ -0,0 +1,43 @@
+extends Node3D
+
+@export_group("zombie")
+@export var spawnable: PackedScene
+@export var spawn_count := 1
+@export var spawn_radius:= 5
+@export var speed:= 3
+@export var player_dist_max := 50
+@export var origin_dist_max := 20
+var player = null
+var rng = RandomNumberGenerator.new()
+@export_group("player")
+@export var player_path : NodePath
+var zombies = []
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	player = get_node(player_path)
+	for i in spawn_count:
+		spawn_zombie()
+
+func spawn_zombie():
+	var newzombie = spawnable.instantiate()
+	add_child(newzombie)
+	var randx = rng.randf_range(position.x-spawn_radius,position.x+spawn_radius)
+	var randz = rng.randf_range(position.z-spawn_radius,position.z+spawn_radius)
+	newzombie.position = Vector3(randx,position.y,randz)
+	newzombie.player_path = player_path
+	newzombie.player = get_node(player_path)
+	newzombie.SPEED = speed
+	newzombie.player_dist_max = player_dist_max
+	newzombie.origin_dist_max = origin_dist_max
+	zombies.append(newzombie)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	#if len(zombies) < 3:
+		#spawn_zombie()
+
+
+func _on_player_3d_player_death() -> void:
+	pass # Replace with function body.
--- scripts/player/new_player_controller.gd
+++ scripts/player/new_player_controller.gd
@@ -6,9 +6,12 @@ extends CharacterBody3D
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export var cam: Camera3D
 @export var flashlight: SpotLight3D
+var alive:=true
 @export_group("headbob")
 @export var headbob_freq := 2.0
 @export var headbob_amplitude := 0.04
+
+@onready var deathscreen: Sprite2D = $deathshow
 
 @export_group("Stamina")
 @export var max_stamina := 100.0
@@ -22,6 +25,7 @@ var cooldown_timer := 0.0
 
 signal sprint_started
 signal sprint_stopped
+signal player_death
 
 @onready var footstep_player_indoor: AudioStreamPlayer3D = $FootstepPlayerIndoors
 @onready var footstep_player_concrete: AudioStreamPlayer3D = $FootstepPlayerConcrete
@@ -289,3 +293,12 @@ func _on_area_3d_area_entered(_area: Area3D) -> void:
 
 func _on_ok_button_pressed() -> void:
 	pass
+	
+func die():
+	print("player exploded unlucky")
+	set_process_input(false)
+	deathscreen.visible = true
+	player_death.emit()
+	
+func _on_hitbox_body_entered(body):
+	die()
